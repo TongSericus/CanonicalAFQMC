8c13b5c80b691fb98c388002f5e07fab9745a48c
TongSericus <45701070+TongSericus@users.noreply.github.com>
2019-10-07 15:38:43 -0400

    Add files via upload

---
+++ MeanFieldSolver.jl
@@ -0,0 +1,20 @@
+function mean_field_solver()
+    convergence_tolerance = 1e-6
+    kinetic_val = kinetic_matrix()
+    n_set = ones(num_sites) / num_sites
+    # each element in the array represents the density of particles on each site
+    trial_matrix = kinetic_val + Diagonal(n_set * U)
+    n_set_old = zeros(num_sites)
+    while sum(abs.(n_set_old - n_set)) > convergence_tolerance
+        energy_spectrum, basis_vector = eigen(trial_matrix)
+        density_matrix = zeros(num_sites, num_sites)
+        n_set_old = n_set
+        Z = sum(exp.(- β * energy_spectrum))
+        for i = 1:num_sites
+            density_matrix += (exp.(- β * energy_spectrum[i])  / Z) * (basis_vector[:, i] * basis_vector[:, i]')
+        end
+        n_set = diag(density_matrix)
+        trial_matrix = kinetic_val + Diagonal(n_set * U)
+    end
+    return n_set
+end
